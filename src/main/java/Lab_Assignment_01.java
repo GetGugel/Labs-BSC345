2cab406eec300987263861d19686446ade841e0f
Brett Gugel <fgugel5678@gmail.com>
2021-09-13 20:02:48 -0400

    This code will test the values between 0-100 and print all even numbers Using a For loop to test each number, and an If statement to determine if the value is positive.

---
+++ src/main/java/Lab_Assignment_01.java
@@ -0,0 +1,11 @@
+
+public class Lab_Assignment_01 {
+    public static void main(String[] args) {
+        System.out.println("All even Numbers 0-100");
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2==0) {//finds and prints even numbers
+                System.out.println(i);   
+            }
+        }
+    }
+}
